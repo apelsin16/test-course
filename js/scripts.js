91c8186790c46c69d1a771c2c29a10050f270530
Apelsin16 <apelsin@i.ua>
2019-09-24 16:20:47 +0300

    start

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
-console.log("CONSOLE");
+
 (function() {
-    document.getElementById("demo").innerHTML = "Screen Width: " + screen.width;
+    const ytplayer = document.getElementById('ytplayer');
+    if ( window.innerWidth < 600  ) {
+        ytplayer.width = '320';
+        ytplayer.height = '180';
+    } else if (window.innerWidth > 601 && window.innerWidth < 800) {
+        ytplayer.width = '580';
+        ytplayer.height = '326';
+    } else {
+        ytplayer.width = '800';
+        ytplayer.height = '450';
+    }
 })();
